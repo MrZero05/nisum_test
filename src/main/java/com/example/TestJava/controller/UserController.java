984de1593b96d8d03b65177d5d729419cdca032d
joseosorio46 <jose.osorio@softwareone.com>
2024-07-17 15:48:46 -0500

    Configurations for swagger

--- src/main/java/com/example/TestJava/controller/UserController.java
+++ src/main/java/com/example/TestJava/controller/UserController.java
@@ -3,9 +3,10 @@ package com.example.TestJava.controller;
 import com.example.TestJava.dto.RequestUserDto;
 import com.example.TestJava.dto.ResponseUserDto;
 import com.example.TestJava.dto.UpdateRequestUserDto;
-import com.example.TestJava.model.User;
 import com.example.TestJava.service.IUserService;
-import com.example.TestJava.service.impl.UserServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ import java.util.UUID;
 
 @Controller
 @RequestMapping("/nisumtest/")
+@Api(value = "User Management System", tags = "User")
 public class UserController {
 
     private final IUserService userService;
@@ -26,14 +28,22 @@ public class UserController {
     }
 
     @PostMapping("/createUser")
-    public ResponseEntity<ResponseUserDto> createUser(@RequestBody RequestUserDto requestUserDto) {
+    @ApiOperation(value = "Create a new user", response = ResponseUserDto.class)
+    public ResponseEntity<ResponseUserDto> createUser(
+            @ApiParam(value = "User details to be created", required = true)
+            @RequestBody RequestUserDto requestUserDto) {
         ResponseUserDto responseUserDto = userService.createUser(requestUserDto);
 
         return ResponseEntity.ok(responseUserDto);
     }
 
     @PatchMapping("/{uuid}")
-    public ResponseEntity<?> updateUser(@PathVariable("uuid") UUID id, @RequestBody UpdateRequestUserDto requestUserDto) {
+    @ApiOperation(value = "Update an existing user", response = ResponseUserDto.class)
+    public ResponseEntity<?> updateUser(
+            @ApiParam(value = "UUID of the user to be updated", required = true)
+            @PathVariable("uuid") UUID id,
+            @ApiParam(value = "User details to be updated", required = true)
+            @RequestBody UpdateRequestUserDto requestUserDto) {
         try {
             ResponseUserDto updatedUser = userService.updateUser(id, requestUserDto);
             return new ResponseEntity<>(updatedUser, HttpStatus.OK);
